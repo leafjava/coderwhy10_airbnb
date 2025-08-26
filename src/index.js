26d6f940ffd1ac8f0811da70968893976999ad11
LAPTOP-KCF3SC5G\HUAWEI <a894074295@126.com>
2025-08-26 10:50:34 +0800

    1614--day89_Redux和18新增的Hooks-项目搭建_16_(掌握)项目搭建-项目状态管理redux的配置(二)

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React,{Suspense} from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 
+import {Provider} from "react-redux"
 import App from './App';
 import "normalize.css"
 import './assets/css/index.less'
+import store from './store'
 
 // @ => src:webpack
 // 问题:react用脚手架隐藏webpack
@@ -15,9 +17,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Suspense fallback="loading">
-      <HashRouter>
-        <App />
-      </HashRouter>
+      <Provider store={store}>
+        <HashRouter>
+          <App />
+        </HashRouter>
+      </Provider>
     </Suspense>
   </React.StrictMode>
 );
