100983ffe961581e1d2dc36a073e13daf6152c09
LAPTOP-KCF3SC5G\HUAWEI <a894074295@126.com>
2025-08-26 15:10:53 +0800

    1618--day90_爱彼迎-头部布局-轮播-集成Redux-展示列表_02_(掌握)项目-Header的封装和整体布局的搭建

--- src/components/app-header/index.jsx
+++ src/components/app-header/index.jsx
@@ -1,8 +1,19 @@
 import React, { memo } from 'react'
+import { HeaderWrapper } from './style'
+import HeaderLeft from './c-cpns/header-left'
+import HeaderCenter from './c-cpns/header-center'
+import HeaderRight from './c-cpns/header-right'
 
 const AppHeader = memo(() => {
   return (
-    <div>AppHeader</div>
+    <HeaderWrapper>
+      {/* <div className="left">left</div>
+      <div className="center">center</div>
+      <div className="right">right</div> */}
+      <HeaderLeft/>
+      <HeaderCenter/>
+      <HeaderRight/>
+    </HeaderWrapper>
   )
 })
 
