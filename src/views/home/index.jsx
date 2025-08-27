9698cab87a466348d4e45ea4743fc18fe2ca83ce
LAPTOP-KCF3SC5G\HUAWEI <a894074295@126.com>
2025-08-27 10:55:53 +0800

    1625--day90_爱彼迎-头部布局-轮播-集成Redux-展示列表_09_(掌握)项目-首页-顶部轮播图图片的展示

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,42 +1,12 @@
-import React, { memo,useEffect, useState } from 'react'
-import hyRequest from '@/services'
+import React, { memo } from 'react'
+import { HomeWrapper } from './style'
+import HomeBanner from './c-cpns/home-banner'
 
 const Home = memo(() => {
-  // 定义状态
-  const [ highScore,setHighScore ] = useState({
-
-  })
-
-  // 网络请求的代码
-  useEffect(()=> {
-    hyRequest.get({url:"/home/highscore"}).then(res => {
-      console.log(res)
-      setHighScore(res)
-    })
-  },[])
-
   return (
-    <div>
-      <h2>{highScore.title}</h2>
-      <h4>{highScore.subtitle}</h4>
-      <ul>
-        {/* {
-          highScore.list && <ul>
-            {
-              highScore.list.map((item) => {
-                return <li key={item.id}>{item.name}</li>
-              })
-            }
-          </ul>
-        } */}
-        {
-          highScore.list?.map((item) => {
-            return <li key={item.id}>{item.name}</li>
-          })
-        }
-
-      </ul>
-    </div>
+    <HomeWrapper>
+      <HomeBanner/>
+    </HomeWrapper>
   )
 })
 
