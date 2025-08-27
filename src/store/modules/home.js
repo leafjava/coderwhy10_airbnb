f6e83c111749e6965d9bd60b8dde4480e25c975a
LAPTOP-KCF3SC5G\HUAWEI <a894074295@126.com>
2025-08-27 11:25:37 +0800

    1626--day90_爱彼迎-头部布局-轮播-集成Redux-展示列表_11_(掌握)项目-首页-高性价比数据Redux获取和管理

--- src/store/modules/home.js
+++ src/store/modules/home.js
@@ -1,14 +1,30 @@
-import {createSlice} from "@reduxjs/toolkit"
+import {createSlice,createAsyncThunk} from "@reduxjs/toolkit"
+import {getHomeGoodPriceData} from '@/services'
 
+export const fetchHomeDataAction =  createAsyncThunk("fetchdata",async (payload)=>{
+  const res = await getHomeGoodPriceData()
+  return res
+
+})
 
 const homeSlice = createSlice({
   name:"home",
   initialState:{
-    productList:[]
+    goodPriceInfo:{}
   },
   reducers:{
-
+    changeGoodPriceInfoAction(state,{payload}){
+      state.goodPriceInfo = payload
+    }
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchHomeDataAction.fulfilled, (state, action) => {
+      console.log(action.payload);
+      state.goodPriceInfo = action.payload;
+    });
   }
 })
+
+export const {changeGoodPriceInfoAction} = homeSlice.actions
 
 export default homeSlice.reducer
--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,11 +1,36 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect } from 'react'
+import { shallowEqual, useDispatch, useSelector } from 'react-redux'
+
+import { fetchHomeDataAction } from '@/store/modules/home'
 import { HomeWrapper } from './style'
 import HomeBanner from './c-cpns/home-banner'
 
+
 const Home = memo(() => {
+  // 从redux中获取数据
+  const {goodPriceInfo} = useSelector((state)=> ({
+    goodPriceInfo:state.home.goodPriceInfo
+  }),shallowEqual)
+
+  // 派发异步事件:发送网络请求
+  const dispatch = useDispatch()
+  useEffect(()=>{
+    dispatch(fetchHomeDataAction())
+  },[dispatch])
+
   return (
     <HomeWrapper>
       <HomeBanner/>
+      <div className='content'>
+        <h2>{goodPriceInfo.title}</h2>
+        <ul>
+          {
+            goodPriceInfo.list.map(item => {
+              return <li key={item.id}>{item.name}</li>
+            })
+          }
+        </ul>
+      </div>
     </HomeWrapper>
   )
 })
