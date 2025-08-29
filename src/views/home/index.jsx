43420cdce88a35cec0e3c52d763ed6874b5e935f
LAPTOP-KCF3SC5G\HUAWEI <a894074295@126.com>
2025-08-29 09:33:26 +0800

    1632--day90_爱彼迎-头部布局-轮播-集成Redux-展示列表_18_(掌握)项目-首页-房间列表的组件代码重构

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -5,7 +5,7 @@ import { fetchHomeDataAction } from '@/store/modules/home'
 import { HomeWrapper } from './style'
 import HomeBanner from './c-cpns/home-banner'
 import SectionHeader from '@/components/section-header'
-import RoomItem from '@/components/room-item'
+import SectionRooms from '@/components/section-rooms'
 
 
 
@@ -27,13 +27,14 @@ const Home = memo(() => {
       <div className='content'>
         <div className='good-price'>
           <SectionHeader title={goodPriceInfo.title}/>
-          <ul className='room-list'>
+          {/* <ul className='room-list'>
             {
               goodPriceInfo.list?.slice(0,8).map(item => {
                 return <RoomItem itemData={item} key={item.id}/>
               })
             }
-          </ul>
+          </ul> */}
+          <SectionRooms roomList={goodPriceInfo.list}/>
         </div>
       </div>
       
